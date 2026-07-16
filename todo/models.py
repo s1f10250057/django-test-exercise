8d8ac73aaa151a2885fe4928a48ec6153ba082d3
Sosuke Nagao <n@NPC-Mac.local>
2026-07-16 13:38:14 +0900

    タグ機能とテストを追加

--- todo/models.py
+++ todo/models.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 
 class Task(models.Model):
     title = models.CharField(max_length=100)
+    tag = models.CharField(max_length=50, blank=True)
     completed = models.BooleanField(default=False)
     posted_at = models.DateTimeField(default=timezone.now)
     due_at = models.DateTimeField(null=True, blank=True)
