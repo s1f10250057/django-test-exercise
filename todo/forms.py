6af6bbe40e7770027c2da92cf74f5260f6cdd002
1F10250179 <s1F102501798@iniad.org>
2026-07-17 02:26:53 +0900

    fix(notifications): 期限変更時に通知状態を戻す

--- todo/forms.py
+++ todo/forms.py
@@ -13,6 +13,15 @@ class TaskForm(forms.ModelForm):
             (Task.RECURRENCE_MONTHLY, '毎月'),
         ]
 
+    def save(self, commit=True):
+        task = super().save(commit=False)
+        if 'due_at' in self.changed_data:
+            task.notified_at = None
+        if commit:
+            task.save()
+            self.save_m2m()
+        return task
+
     class Meta:
         model = Task
         fields = (
